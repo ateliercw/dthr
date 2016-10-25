171da47f61f2ad66795eb8e16cfdd6ac1e485351
Michael Skiba <mike.skiba@raizlabs.com>
2016-10-24 22:23:19 -0400

    Initial Commit

---
+++ dthr/dthr.h
@@ -0,0 +1,19 @@
+//
+//  dthr.h
+//  dthr
+//
+//  Created by Michael Skiba on 10/24/16.
+//  Copyright © 2016 Michael Skiba. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for dthr.
+FOUNDATION_EXPORT double dthrVersionNumber;
+
+//! Project version string for dthr.
+FOUNDATION_EXPORT const unsigned char dthrVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <dthr/PublicHeader.h>
+
+
